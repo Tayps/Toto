f363235f12212eec72f93dd4012f969cf24f1dd9
Tayps <taypin.zb@gmail.com>
2022-02-11 00:26:39 +0800

    Test Push to Github

--- TOTO.py
+++ TOTO.py
@@ -17,14 +17,18 @@ for tr in namerows:
     td = tr.find_all('td')
     row = [tr.text.strip() for tr in td]
     l.append(row)
-df = pd.DataFrame(l, columns=['Draw', 'Date', 'Winning_No', 'Addl_No', 'From_Last', 'Sum', 'Average', 'Odd_Even', '1-10', '11-20', '21-30', '31-40', '41-50'])
+df = pd.DataFrame(l, columns=['Draw', 'Date', 'Winning_No', 'Addl_No', 'From_Last',
+                  'Sum', 'Average', 'Odd_Even', '1-10', '11-20', '21-30', '31-40', '41-50'])
 df = df.iloc[2:]
 # df[['A', 'B', 'C', 'D', 'E', 'F',]] = df.Winning_No.str.split(",",expand=True)
-df = df.set_index('Draw').Winning_No.str.split(",",expand=True).stack()
-df = pd.get_dummies(df, prefix = 'Number').groupby(level=0).sum()
+df = df.set_index('Draw').Winning_No.str.split(",", expand=True).stack()
+df = pd.get_dummies(df, prefix='Number').groupby(level=0).sum()
 
 freq_draws = apriori(df, min_support=0.018, use_colnames=True)
-rules = association_rules(freq_draws, metric = "lift", min_threshold=1)
+rules = association_rules(freq_draws, metric="lift", min_threshold=1)
 pd.set_option('display.max_columns', None)
 rules.head()
 rules.to_csv('toto_mba.csv')
+
+
+# Test comment
